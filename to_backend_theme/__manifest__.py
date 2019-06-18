bb92d249c2d611174264a5cbc8323f30137269d1
David Tran <david.tran@tvtmarine.com>
2019-06-18 20:03:02 +0700

    [FIX] to_backend_theme: wrong category

--- to_backend_theme/__manifest__.py
+++ to_backend_theme/__manifest__.py
@@ -5,11 +5,11 @@
 {
     "name": "ERPOnline Backend Theme",
     "summary": "Mobile backend theme for Odoo community",
-    "version": "10.0.1.0.23",
-    "category": "Theme/Backend",
+    "version": "11.0.1.0.23",
+    "category": "Website/Theme/Backend",
     'author' : 'Openworx, T.V.T Marine Automation (aka TVTMA)',
     'website': 'https://www.erponline.vn',
-    'live_test_url': 'https://v10demo-int.erponline.vn',
+    'live_test_url': 'https://v11demo-int.erponline.vn',
     'support': 'support@ma.tvtmarine.com',
 	"description": """
 		Backend theme for Odoo community edition, based on the Material/United Backend Theme by Openworx
@@ -19,14 +19,16 @@
 	],
     "license": "LGPL-3",
     "installable": True,
+    "auto_install": True,
     "depends": [
-	'web_responsive',
+        'web_responsive',
     ],
     "data": [
         'views/assets.xml',
         'views/res_company_view.xml',
         'views/users.xml',
         'views/sidebar.xml',
+        'views/web.xml',
     ],
 }
 
