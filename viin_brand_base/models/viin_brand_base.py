cfd3381003f645a618ad7bf5df5e9a8350bfa396
hieuld <hrcp.hieu@gmail.com>
2020-09-12 11:20:13 +0700

    [IMP] Re-commit change comment in func update_module_icon and update_menu_icon

--- viin_brand_base/models/viin_brand_base.py
+++ viin_brand_base/models/viin_brand_base.py
@@ -21,15 +21,15 @@ class ViinBrandBase(models.AbstractModel):
         module._get_icon_image()
         
     def update_menu_icon(self, origin_module_name, menu_xml_id, icon_path = 'static/description/icon.png'):
-        '''
-        This method used to set origin menu module's icon by brand menu module's icon and restore after brand module uninstalled
+        """
+        This method used to set origin menu app icon by brand menu app icon and restore after brand module uninstalled
         
-        @param origin_module_name: module name needed to replace icon
+        @param origin_module_name: module name needed to replace menu app icon
         @param menu_xml_id: full xml id of menuitem web_icon, example: crm.crm_menu_root
         @param icon_path: default: static/description/icon.png
         
         If it is not passed then it means the method is used to restore module icon to origin
-        '''
+        """
 
         module_icon_menu = self.env['ir.ui.menu'].search([('id', '=', self.env.ref(menu_xml_id).id)], limit=1)
         module_icon_menu.write({'web_icon' : origin_module_name + ',' + icon_path})
