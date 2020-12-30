3eb3c266e8edc45dfedf4c3cc24272768f35d2a6
TranNgocSon1996 <transon821996@gmail.com>
2020-12-30 16:26:40 +0700

    [ADD] viin_brand_eos: replace brand Odoo

---
+++ viin_brand_eos/__manifest__.py
@@ -0,0 +1,52 @@
+{
+    'name': "Viindoo EOS Branding",
+    'name_vi_VN': "Thương hiệu Viindoo EOS",
+
+    'summary': """
+Set Viindoo EOS Branding for email.
+""",
+
+    'summary_vi_VN': """
+Thiết lập thương hiệu Viindoo EOS cho email
+        """,
+
+    'description': """
+This module change some information for Viindoo branding
+ 
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+
+    """,
+
+    'description_vi_VN': """
+Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu Viindoo
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+
+    """,
+
+    'author' : 'T.V.T Marine Automation (aka TVTMA)',
+    'website': 'https://www.viindoo.com',
+    'live_test_url': 'https://v13demo-int.erponline.vn',
+    'support': 'support@ma.tvtmarine.com',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/tvtma/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Hidden',
+    'version': '0.1',
+    
+    # any module necessary for this one to work correctly
+    'depends': ['mail'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 99.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
