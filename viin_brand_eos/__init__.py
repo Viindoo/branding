fce5fa37800666d433af1d1e8e3b334ec99601e4
TranNgocSon1996 <transon821996@gmail.com>
2021-01-15 16:57:39 +0700

    [IMP] viin_brand: move viin_brand_eos to viin_brand

--- viin_brand_eos/__init__.py
+++
@@ -1,8 +0,0 @@
-from odoo import SUPERUSER_ID, api
-from . import models
-
-
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['mail.template'].search([]).replace_brand()
-    env['ir.ui.view'].search([]).replace_brand()
