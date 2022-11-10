39c59f3dbb7e88a7bbc4fe96e68dacb742bcedcb
dongpt311099 <phanthanhdong311099@gmail.com>
2022-11-10 13:42:21 +0700

    [UPG] viin_brand_im_livechat: Upgrade module

--- viin_brand_im_livechat/__manifest__.py
+++ viin_brand_im_livechat/__manifest__.py
@@ -48,14 +48,14 @@ Editions Supported
     'data': [
         # 'security/ir.model.access.csv',
         'views/im_livechat_channel_templates.xml',
-        'views/im_livechat_channel_views.xml'
+        'views/im_livechat_channel_views.xml',
     ],
     'images': [
     	# 'static/description/main_screenshot.png'
     	],
-    'installable': False,
+    'installable': True,
     'application': False,
-    'auto_install': False, # set True after upgrade 16.0
+    'auto_install': True,
     'price': 0.0,
     'currency': 'EUR',
     'license': 'OPL-1',
