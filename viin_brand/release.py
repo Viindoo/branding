e2d34e3c7ecb867113ac977b864824cc3dc397fc
Cindy Nguyen <kimloan.vnua@gmail.com>
2022-12-07 16:21:28 +0700

    [IMP] viin_brand: Viindoo debranding

---
+++ viin_brand/release.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import odoo
+
+
+odoo.release.product_name = 'Viindoo'
+odoo.release.description = 'Viindoo Server'
+odoo.release.long_desc = '''Viindoo is a complete ERP and CRM. The main features are accounting (analytic
+and financial), stock management, sales and purchases management, tasks
+automation, marketing campaigns, help desk, POS, etc. Technical features include
+a distributed server, an object database, a dynamic GUI,
+customizable reports, and XML-RPC interfaces.
+'''
+odoo.release.url = 'https://viindoo.com'
+odoo.release.author = 'Viindoo'
+odoo.release.author_email = 'apps.support@viindoo.com'
