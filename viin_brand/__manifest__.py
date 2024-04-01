b538fa730784b5529d4c82592917bb81d75d727a
Roy Le <43790414+royle-viindoo@users.noreply.github.com>
2024-04-01 13:18:48 +0700

    Update viin_brand/__manifest__.py

--- viin_brand/__manifest__.py
+++ viin_brand/__manifest__.py
@@ -41,7 +41,6 @@ Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu
     'data': [
     ],
     'installable': True,
-    'application': False,
     'auto_install': True,
     'price': 9.9,
     'currency': 'EUR',
